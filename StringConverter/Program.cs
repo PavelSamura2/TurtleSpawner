9894995cd1fceb12bcf0d6cd63959ad9d9158a7b
Pavel <pasha.splav22@gmail.com>
2021-06-16 20:23:11 +0700

    #commit: Finalize logic

--- StringConverter/Program.cs
+++ StringConverter/Program.cs
@@ -1,57 +1,76 @@
 ﻿using System;
-using System.Collections.Generic;
 
 namespace StringConverter
 {
+    //Интерфейс, на случай если нужно будет реализовать несколько разных конвертеров
     internal interface IConverter
-    {
-        bool FindElementCondition(String InputString, char primitiveSymbol);
-        string PerformConvertationJob(String inputString);
+    { 
+        void PerformConvertJob(ref string inputString);
     }
 
+    //Конвертер, который заменяет символ на '(', если символ встречается 1 раз, на ')', если несколько
     internal class StringConverterClass : IConverter
     {
-        public bool FindElementCondition(String InputString, char primitiveSymbol)
+        //Метод, производящий конвертацию строки и пишет результат в outString. Асимптотически работает за O(L)
+        //где L - длина строки. Достигается такая быстрота использованием таблицы вхождений по ASCII символам
+        public void PerformConvertJob(ref string outString)
         {
-            int counterPrimitiveCondition = 0;
-            foreach (var value in InputString)
-            {
-                if (value == primitiveSymbol) 
-                    counterPrimitiveCondition++;
-            }
-
-            if (counterPrimitiveCondition > 1)
-                return true;
-            else
-                return false;
-        }
-
-        public string PerformConvertationJob(String inputString)
-        {
-            string outFormattedString = "";
-
-            foreach (var currentPrimitive in inputString)
-            {
-                if (FindElementCondition(inputString, currentPrimitive))
-                    outFormattedString += ")";
-                else
-                    outFormattedString += "(";
-            }
-            return outFormattedString;
+            //Объявляем массив размером с алфавит (500 хватит на цифры, буквы, специальные символы с запасом. 
+            //Этот массив будет использоваться как счетчик-таблица, где по номеру символа ASCII можно получить 
+            //количество вхождений символа с таким ASCII кодом
+            var symbolCountArray = new int[500];
             
+            //Создаем строку для работы внутри функции. Такое использование добавляет безопасности
+            var internalUserString = outString.ToLower();
+            //Выходная строка
+            var resultString = "";
+
+            //Делаем инкремент к каждому ASCII символу в таблицу symbolContArray. Конвертация от char к int происходит
+            //автоматически
+            foreach (char c in internalUserString)
+            {
+                symbolCountArray[c]++;
+            }
+
+            //Проходимся по всей строке и делаем конкатенацию нужной скобочки в зависимости от количества вхождений
+            //символа в таблицу
+            foreach (char c in internalUserString)
+            {
+                if (symbolCountArray[c] > 1)
+                    resultString += ')';
+                else
+                    resultString += '(';
+            }
+
+            //Записываем в выходной аргумент нашу строку
+            outString = resultString;
         }
     }
 
 
-class Program
+    internal static class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            Console.WriteLine("Press enter input string: ");
-            var inputString = Console.ReadLine();
+            //Считываем количество строк, которые потребуем ввести у пользователя
+            Console.WriteLine("Enter strings count: ");
+            var inputStringsCount  = Convert.ToInt32(Console.ReadLine());
             
+            //Создаем конвертер строк
             var converter = new StringConverterClass();
-            Console.WriteLine("Formatted string: " + converter.PerformConvertationJob(inputString));
+
+            for (int queryIndex = 0; queryIndex < inputStringsCount; ++queryIndex)
+            {
+                //Считываем пользовательскую строку
+                Console.WriteLine("Press enter input string: ");
+                var userString = Console.ReadLine();
+            
+                //Делаем конвертацию строки
+                converter.PerformConvertJob(ref userString);
+            
+                //Выводим результат конвертации
+                Console.WriteLine($"Formatted string: {userString}");
+            }
         }
     }
 }
